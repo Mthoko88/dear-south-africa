465430ac68481b388f3a3462ae8dbd169d0932b0
Phillip Dube <105677027+Mthoko88@users.noreply.github.com>
2026-04-30 09:03:34 +0200

    Update story-card.tsx

--- components/story-card.tsx
+++ components/story-card.tsx
@@ -650,7 +650,7 @@ export function StoryCard({ story }: StoryCardProps) {
                   </Badge>
                 )}
               </div>
-              <div className=" items-end">            
+              <div className="text-xs items-end">            
               {story.organisations && (
                           <div onClick={(e) => e.stopPropagation()}>
                             <OrganisationFollowButton 
