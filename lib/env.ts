80de635df378e3c107f84c961173a8495df4a5a6
Phillip Dube <105677027+Mthoko88@users.noreply.github.com>
2026-05-15 07:57:43 +0200

    Create env.ts

---
+++ lib/env.ts
@@ -0,0 +1,12 @@
+export const env = {
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+}
+
+if (!env.supabaseUrl) {
+  throw new Error("Missing NEXT_PUBLIC_SUPABASE_URL")
+}
+
+if (!env.supabaseAnonKey) {
+  throw new Error("Missing NEXT_PUBLIC_SUPABASE_ANON_KEY")
+}
