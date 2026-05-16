67dd8a66d809d5f090707a6f8b54d0c5b34951f9
Phillip Dube <105677027+Mthoko88@users.noreply.github.com>
2026-05-16 19:24:12 +0200

    Update env.ts

--- lib/env.ts
+++ lib/env.ts
@@ -2,6 +2,3 @@ export const env = {
   supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL || "",
   supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
 }
-
-export const isSupabaseConfigured =
-  !!env.supabaseUrl && !!env.supabaseAnonKey
