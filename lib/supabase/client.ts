5c5782a727e21064dda4dde3d6264d61478febeb
Phillip Dube <105677027+Mthoko88@users.noreply.github.com>
2026-05-16 17:25:14 +0200

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,17 +1,11 @@
-import { createBrowserClient } from '@supabase/ssr'
-import type { SupabaseClient } from '@supabase/supabase-js'
+"use client"
 
-let client: SupabaseClient | null = null
+import { createBrowserClient } from "@supabase/ssr"
 
-export function createClient() {
-  if (client) {
-    return client
-  }
-  
-  client = createBrowserClient(
+export const createClient = () =>
+  createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
-  
-  return client
-}
+
+export const supabase = createClient()
