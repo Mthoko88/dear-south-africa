13bb4dde0b3b6a3203ce8a165a05e6d39ccb0b26
Phillip Dube <105677027+Mthoko88@users.noreply.github.com>
2026-05-16 17:22:51 +0200

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -7,11 +7,11 @@ export function createClient() {
   if (client) {
     return client
   }
-
+  
   client = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   )
-
+  
   return client
 }
